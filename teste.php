8126d406fcfbb5c1a70012287c0244d8ec35ec15
VirsoKuhl <vilsoncbk@gmail.com>
2025-12-15 19:52:16 -0300

    teste-de-um-programador

---
+++ teste.php
@@ -0,0 +1,9 @@
+<?php
+    function somarTresNumeros($n1, $n2 = 2, $n3 = 3) {
+        return $n1 + $n2 + $n3;
+    }
+
+    
+    $resultado = somarTresNumeros(5, 4);
+    echo "A soma é: $resultado";
+?>
